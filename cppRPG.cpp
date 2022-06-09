bf598fba46dac0e5749b03c9c3185d75aedcd3be
Robert Dawid <robinxon.pl@gmail.com>
2022-06-09 23:20:26 +0200

    Dodano pętlę wyboru opcji w menu głównym

--- cppRPG.cpp
+++ cppRPG.cpp
@@ -4,13 +4,35 @@ using namespace std;
 
 void menuGlowne()
 {
-    //zapis ciągu znaków w postaci R"(ciąg znaków)" nie wymaga używania znaku ucieczki
-    cout << R"(                    ____________________  ________ )" << endl;
-    cout << R"(  ____ ______ ______\______   \______   \/  _____/ )" << endl;
-    cout << R"(_/ ___\\____ \\____ \|       _/|     ___/   \  ___ )" << endl;
-    cout << R"(\  \___|  |_> >  |_> >    |   \|    |   \    \_\  \)" << endl;
-    cout << R"( \___  >   __/|   __/|____|_  /|____|    \______  /)" << endl;
-    cout << R"(     \/|__|   |__|          \/                  \/ )" << endl;
+    int wybor = 0;
+    do
+    {
+        system("CLS");
+        //zapis ciągu znaków w postaci R"(ciąg znaków)" nie wymaga używania znaku ucieczki
+        cout << R"(                    ____________________  ________ )" << endl;
+        cout << R"(  ____ ______ ______\______   \______   \/  _____/ )" << endl;
+        cout << R"(_/ ___\\____ \\____ \|       _/|     ___/   \  ___ )" << endl;
+        cout << R"(\  \___|  |_> >  |_> >    |   \|    |   \    \_\  \)" << endl;
+        cout << R"( \___  >   __/|   __/|____|_  /|____|    \______  /)" << endl;
+        cout << R"(     \/|__|   |__|          \/                  \/ )" << endl;
+        cout << endl;
+        cout << "1. Nowa gra" << endl;
+        cout << "2. Wczytaj gre" << endl;
+        cout << "9. Wyjdz z gry" << endl;
+        cin >> wybor;
+
+        switch (wybor)
+        {
+        case 1:
+            //nowa gra
+            break;
+        case 2:
+            //wczytaj gre
+            break;
+        default:
+            break;
+        }
+    } while (wybor != 9);
 }
 
 int main()
@@ -18,7 +40,6 @@ int main()
     menuGlowne();
 
     Gracz gracz = Gracz("Robert");
-    cout << "Gracz: " << gracz.dostanNazwa() << endl;
     
     return 0;
 }
